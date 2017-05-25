91ebafe9d461339815386b85ee42b9c4484f99d6
jllau <ex_wljr_liujialin@pingan.com.cn>
2017-05-25 15:26:17 +0800

    add redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,14 +4,20 @@ import { routerReducer } from 'react-router-redux'
 //组合之后的reducer
 import reducer from './reducers';
 import hashHistory from 'react-router'
+import { createLogger } from 'redux-logger'
 
+const middleware = [ thunk ];
+console.log(process.env)
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
 //全局唯一的store,挂在window上方便查看
 var store = createStore(
     combineReducers({
         ...reducer,
         routing: routerReducer
     }),
-    applyMiddleware(thunk)
+    applyMiddleware(...middleware)
 );
 
 export default store;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");  //css单独打包
+var ExtractTextPlugin = require("extract-text-webpack-plugin"); //css单独打包
 var HtmlWebpackPlugin = require('html-webpack-plugin'); //生成html
 var webpack = require('webpack');
 
 const path = require('path');
 
 const svgDirs = [
-    require.resolve('antd-mobile').replace(/warn\.js$/, ''),  // 1. 属于 antd-mobile 内置 svg 文件
-    path.resolve(__dirname, 'src/images')  // 2. 自己私人的 svg 存放目录
+    require.resolve('antd-mobile').replace(/warn\.js$/, ''), // 1. 属于 antd-mobile 内置 svg 文件
+    path.resolve(__dirname, 'src/images') // 2. 自己私人的 svg 存放目录
 ];
 
 module.exports = {
@@ -25,8 +25,7 @@ module.exports = {
         chunkFilename: '[name].[chunkhash:5].chunk.js'
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.js$/,
                 use: ['babel-loader'],
                 include: /src/
@@ -52,15 +51,15 @@ module.exports = {
             {
                 test: /\.(svg)$/i,
                 use: 'svg-sprite-loader',
-                include: svgDirs,  // 把 svgDirs 路径下的所有 svg 文件交给 svg-sprite-loader 插件处理
+                include: svgDirs, // 把 svgDirs 路径下的所有 svg 文件交给 svg-sprite-loader 插件处理
             },
         ]
     },
     devServer: {
-        contentBase: './build',  //本地服务器所加载的页面所在的目录
+        contentBase: './build', //本地服务器所加载的页面所在的目录
         port: 8888,
-        historyApiFallback: true,  //不跳转
-        inline: true,  //实时刷新
+        historyApiFallback: true, //不跳转
+        inline: true, //实时刷新
         proxy: {
             '/api/*': {
                 target: 'http://jsonplaceholder.typicode.com/',
@@ -72,6 +71,11 @@ module.exports = {
     plugins: [
         new ExtractTextPlugin({
             filename: 'main.css'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('development')
+            }
         }),
         new webpack.LoaderOptionsPlugin({
             options: {
@@ -86,14 +90,15 @@ module.exports = {
                 }
             }
         }),
-        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+        new HtmlWebpackPlugin({  //根据模板插入css/js等生成最终HTML
             filename: './index.html', //生成的html存放路径，相对于 path
             template: './src/templates/index.html', //html模板路径
-            hash: true,    //为静态资源生成hash值
+            hash: true,
+                //为静态资源生成hash值
         })
     ],
     resolve: {
         modules: ['node_modules', path.join(__dirname, '../node_modules')],
-        extensions: [ '.web.js', '.js', '.json'],
+        extensions: ['.web.js', '.js', '.json'],
     },
 };
